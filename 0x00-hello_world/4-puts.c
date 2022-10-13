f329005be64fa0b85ce75ac2bcbe11cdf03098e5
HayBam <ayobami.odunlami@hotmail.com>
2022-10-13 13:15:50 -0700

    Use puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints str1
+ * Return: Always return zero if successful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
