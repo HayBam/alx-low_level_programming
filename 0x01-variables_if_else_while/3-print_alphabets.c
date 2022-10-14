7f80870ff95a3716440cfd7f0c3fe82327d1e431
HayBam <ayobami.odunlami@hotmail.com>
2022-10-14 06:30:35 -0700

    Print letters in upper and lowercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints output of alphabeth
+ * in small letters
+ *
+ * Return: Always return zero if successful
+ */
+int main(void)
+{
+	char alphabeth = 'a';
+	char alphabeth2 = 'A';
+
+	while (alphabeth <= 'z')
+	{
+		putchar (alphabeth);
+		alphabeth++;
+	}
+	while (alphabeth2 <= 'Z')
+	{
+		putchar (alphabeth2);
+		alphabeth2++;
+	}
+	putchar ('\n');
+	return (0);
+}
