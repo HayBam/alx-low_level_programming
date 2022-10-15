e3a0c88693d3b40497699c3e10ee1d8af4dddcfc
HayBam <ayobami.odunlami@hotmail.com>
2022-10-15 04:59:07 -0700

    Print unique combination of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints the unique combination
+ * of three single digit numbers
+ *
+ * Return: Always return zero if successful
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (c = 48; c <= 57; c++)
+			{
+				if (a < b && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
