bb095b8258766d4c73915299b065493b2caabbae
HayBam <ayobami.odunlami@hotmail.com>
2022-10-18 14:16:27 -0700

    Absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - To check the absolute number of integers
+ *
+ * @n: number to check for
+ *
+ * Return: Always return 1 if int is positive
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
