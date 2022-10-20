af0d2a5a21fc9d79d18613994b0bd77611b39ee1
HayBam <ayobami.odunlami@hotmail.com>
2022-10-20 13:24:27 -0700

    Multiply two integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,15 @@
 /**
+ * mul - This function multiplies two integers
  *
+ * @a: The int data type of the first argument
+ * @b: The int data type of the second argument
  *
+ * Return: Alwasy return z if successful
  */
 int mul(int a, int b)
 {
-	int mul = int a * int b;
-	return(mul);
+	int a, int b, int z;
+
+	z = a * b
+	return (z);
 }
