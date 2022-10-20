7932a27a1242f399c11262ce5ff7122e88a92190
HayBam <ayobami.odunlami@hotmail.com>
2022-10-20 13:09:18 -0700

    Detect digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+
+/**
+ * _isdigit - This function detects digits
+ *
+ * @c: This is the data type for the argument
+ *
+ * Return: Always return zero if successful
+ */
+int _isdigit(int c)
+{
+	if (isdigit(c)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
