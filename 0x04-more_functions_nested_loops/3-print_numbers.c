b2783da1367e28836c34c7b009c283c85b70fb9a
HayBam <ayobami.odunlami@hotmail.com>
2022-10-20 13:54:57 -0700

    Print numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+/**
+ * print_numbers - This function prints the numbers 0 to 9
+ *
+ * Return: Always return zero if successful
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar('i');
+		i++;
+	}
+	_putchar(' ');
+	return (0);
+}
