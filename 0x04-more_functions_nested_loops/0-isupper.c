4adc5d29f19196032229a2b81c7da23fceb2dab0
HayBam <ayobami.odunlami@hotmail.com>
2022-10-20 12:21:52 -0700

    Add function for uppercase detection

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+
+/**
+ * main - This function checks for uppercase character
+ *
+ * Returns: Always return zero if successful
+ */
+int _isupper(int c)
+{
+	isupper(c);
+
+	if (c == 1)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This project contains function and nested loops
