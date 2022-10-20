4689a78066c7b3be38d23965b9beab515e720bf7
HayBam <ayobami.odunlami@hotmail.com>
2022-10-20 01:01:53 -0700

    Add if else statement to caputure invalid days for a month

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,12 +12,18 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) || (year % 100 == 0))
 	{
-		if (month >= 2 && day >= 60)
+		if ((month == 2 && day > 60) || (month == 4 && day > 120)
+				|| (month == 6 && day > 181) || (month == 9 && day > 273)
+				|| (month == 11 && day > 334))
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else if (month >= 2 && day >= 60)
 		{
 			day++;
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 366 - day);
 		}
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
@@ -25,8 +31,9 @@ void print_remaining_days(int month, int day, int year)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
-		else if ((month == 4 && day > 120) || (month == 6 && day > 181)
-				|| (month == 9 && day > 273) || (month == 11 && day > 334))
+		else if ((month == 2 && day > 60) || (month == 4 && day > 120) ||
+				(month == 6 && day > 181) || (month == 9 && day > 273) ||
+				(month == 11 && day > 334))
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
