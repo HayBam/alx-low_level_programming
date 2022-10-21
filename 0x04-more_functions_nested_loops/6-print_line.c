f43cd851b6ff1db2ea5a1ecc28e9159743b965f2
HayBam <ayobami.odunlami@hotmail.com>
2022-10-21 13:08:17 -0700

    Prints underscore per number

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_line - This function prints _ everytime
+ * there is a number
+ *
+ * Return: Always return zero if successful
+ */
+void print_line(int n)
+{
+	int i;
+	
+	i = 0;
+	if (n > i)
+	{
+		while (n > i)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+	else if (n <= i)
+	{
+		_putchar('\n');
+	}
+	return (n);
+}
