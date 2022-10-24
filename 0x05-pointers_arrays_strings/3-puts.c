073372dff84b39fc724e00368eddbe1b8122c68f
HayBam <ayobami.odunlami@hotmail.com>
2022-10-24 13:59:21 -0700

    Funtion to print a string in stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdout
+ *
+ * @str: Pointer to the string to print
+ */
+void _puts(char *str)
+{
+	int a;
+	
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
