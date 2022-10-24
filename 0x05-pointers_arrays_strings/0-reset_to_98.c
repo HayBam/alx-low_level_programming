b34e033d56cade150b7f58b1470631f16bb1bae1
HayBam <ayobami.odunlami@hotmail.com>
2022-10-24 11:32:32 -0700

    Remove trailing white space

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * void reset_to_98 - This function takes a pointer to an int
+ * reset_to_98 - This function takes a pointer to an int
  * and updates the value it points to, to 98
+ *
+ * @n: This is the pointer for integer n
  *
  * Return: ALways return zero if successful
  */
 void reset_to_98(int *n)
 {
 	int *a;
-	
+
 	a = &n;
 	*a = 98;
 }
