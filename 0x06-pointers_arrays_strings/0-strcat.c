2c2f9ce4d1bc09673ec2e34b1edb396c90dfd608
HayBam <ayobami.odunlami@hotmail.com>
2022-10-28 14:13:28 -0700

    Add two strings together

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
- * *_strcat
+ * _strcat - This function joins two strings
  *
+ * @dest: string to append to
+ * @src: string for addition
  *
+ * Return: dest if succesful
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a, b;
+
+	a = 0;
+	b = 0;
+
+	while (dest[a] != '\0')
+		a++;
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
